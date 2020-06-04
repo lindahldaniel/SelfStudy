5212cf77652014194faf54018e44f0360f872442
Daniel Lindahl <daniel.lindahl@hotmail.com>
2020-06-04 17:48:12 +0200

    MadLibs done

--- MadLibs/MadLibs/Program.cs
+++ MadLibs/MadLibs/Program.cs
@@ -6,7 +6,26 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string color, pluralNoun, celebrity, verb;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.Write("Enter a verb: ");
+            verb = Console.ReadLine();
+
+            Console.WriteLine("\nRoses are " + color);
+            Console.WriteLine(pluralNoun +" are blue");
+            Console.WriteLine("I love " + celebrity);
+            Console.WriteLine("This fucker like to " + verb + ", \njust as much as you.");
+
+            Console.ReadLine();
         }
     }
 }
