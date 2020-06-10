e6fca955d4e907c18e9a65285806cc16179c778b
Daniel Lindahl <daniel.lindahl@hotmail.com>
2020-06-10 09:22:47 +0200

    Eventhandling till Calculator

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,34 +6,42 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Choose between +-/*% ");
-            string mathType = Console.ReadLine();
+            
 
-            Console.Write("Enter a number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter another number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-
-            switch (mathType)
+            try
             {
-                case "-":
-                    Subtraction(num1, num2);
-                    break;
-                case "/":
-                    Division(num1, num2);
-                    break;
-                case "*":
-                    Multiplication(num1, num2);
-                    break;
-                case "%":
-                    Modulus(num1, num2);
-                    break;
+                Console.Write("Choose between +-/*% ");
+                string mathType = Console.ReadLine();
 
-                default:
-                    Addition(num1, num2);
-                    break;
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (mathType)
+                {
+                    case "-":
+                        Subtraction(num1, num2);
+                        break;
+                    case "/":
+                        Division(num1, num2);
+                        break;
+                    case "*":
+                        Multiplication(num1, num2);
+                        break;
+                    case "%":
+                        Modulus(num1, num2);
+                        break;
+
+                    default:
+                        Addition(num1, num2);
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
             }
         }
 
@@ -41,6 +49,7 @@ namespace Calculator
         {
             Console.WriteLine(nr1 + nr2);
         }
+
         static void Subtraction(int nr1, int nr2)
         {
             Console.WriteLine(nr1 - nr2);
